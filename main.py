79c623e762cf0bdd0d8fd82a01f7da68ee76839f
afrida <arahman@mymedicalhub.com>
2022-07-01 22:10:10 +0600

    1 problem solved

--- main.py
+++ main.py
@@ -1,67 +1,29 @@
 from typing import List
 
 
-def validMountainArray1(arr: List[int]) -> bool:
-    sortedList = sorted(arr, reverse=True)
-    b = False
-    for i in range(len(arr)):
-        if i + 1 > len(arr) - 1:
-            break
-        else:
-            if arr[0] == arr[len(arr) - 1]:
-                if arr[i + 1] - arr[i] == 0:
-                    b = False
-                    break
-                elif arr[0] > sortedList[0] and arr[len(arr) - 1] > sortedList[0]:
-                    b = False
-                    break
-                elif arr[0] > sortedList[len(arr) - 1] and arr[len(arr) - 1] > sortedList[len(arr) - 1]:
-                    b = False
-                    break
-                b = True
-            elif arr[0] < sortedList[0] and arr[len(arr) - 1] < sortedList[0]:
-                if arr[i + 1] - arr[i] == 0:
-                    b = False
-                    break
-                elif arr[0] > sortedList[0] and arr[len(arr) - 1] > sortedList[0]:
-                    b = False
-                    break
-                elif arr[0] > sortedList[len(arr) - 1] and arr[len(arr) - 1] > sortedList[len(arr) - 1]:
-                    b = False
-                    break
-                b = True
-            else:
-                b = False
-                break
-    return b
-
-
 def validMountainArray(arr: List[int]) -> bool:
     b = False
-    inc = []
-    dec = []
     a = sorted(arr, reverse=True)
-    for i in arr[:arr.index(a[0])]:
-        inc.append(i)
-    for i in arr[arr.index(a[0]) + 1:]:
-        dec.append(i)
-    if sorted(inc, reverse=False) == inc and sorted(dec, reverse=True) == dec:
-        for i in inc:
-            if i + 1 > len(arr) - 1:
-                break
-            elif arr[i + 1] - arr[i] == 0:
-                b = False
-                break
-        for j in dec:
-            if j + 1 > len(arr) - 1:
-                break
-            elif arr[j + 1] - arr[j] == 0:
-                b = False
-                break
+    inc = [i for i in arr[:(arr.index(a[0]) + 1)]]
+    dec = [i for i in arr[arr.index(a[0]):]]
+
+    print(f'inc={inc}')
+    print(f'dec={dec}')
+    if sorted(inc, reverse=False) == inc and len(inc) > 1 and len(dec) > 1 and sorted(dec, reverse=True) == dec:
         b = True
+        for i in range(len(inc)):
+            if inc.count(inc[i]) > 1:
+                b = False
+                break
+        for i in range(len(dec)):
+            if dec.count(dec[i]) > 1:
+                b = False
+                break
+        if inc.count(0) > 1 and dec.count(0) > 1:
+            b = False
     else:
         b = False
     return b
 
 
-print(validMountainArray([5, 10, 12, 7, 5, 1, 2]))
+print(validMountainArray([2, 1]))
