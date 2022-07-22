c7ccbf22bb631a62bffec61f628eb038d3c1b9f2
afrida <arahman@mymedicalhub.com>
2022-07-22 22:01:01 +0600

    not solved int to roman

--- Easy/RomanToInteger.py
+++ Easy/RomanToInteger.py
@@ -1,21 +1,25 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        final = []
-        symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+def romanToInt(s: str) -> int:
+    final = []
+    symbol = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-        i = 0
-        while i < len(s):
-            if i + 1 > len(s) - 1:
-                temp = symbol[s[i]]
-                final.append(temp)
-                break
-            elif (s[i] == 'I' and s[i + 1] == 'V') or (s[i] == 'I' and s[i + 1] == 'X') \
-                    or (s[i] == 'X' and s[i + 1] == 'L') or (s[i] == 'X' and s[i + 1] == 'C') \
-                    or (s[i] == 'C' and s[i + 1] == 'D') or (s[i] == 'C' and s[i + 1] == 'M'):
-                temp = symbol[s[i + 1]] - symbol[s[i]]
-                i = i + 2
-            else:
-                temp = symbol[s[i]]
-                i += 1
+    i = 0
+    while i < len(s):
+        if i + 1 > len(s) - 1:
+            temp = symbol[s[i]]
             final.append(temp)
-        return sum(final)
+            print(final)
+            break
+        elif (s[i] == 'I' and s[i + 1] == 'V') or (s[i] == 'I' and s[i + 1] == 'X') \
+                or (s[i] == 'X' and s[i + 1] == 'L') or (s[i] == 'X' and s[i + 1] == 'C') \
+                or (s[i] == 'C' and s[i + 1] == 'D') or (s[i] == 'C' and s[i + 1] == 'M'):
+            temp = symbol[s[i + 1]] - symbol[s[i]]
+            i = i + 2
+        else:
+            temp = symbol[s[i]]
+            i += 1
+            print(final)
+        final.append(temp)
+    return sum(final)
+
+
+romanToInt("III")
---
+++ Easy/mainEasy.py
@@ -0,0 +1,45 @@
+class Solution:
+    def intToRoman(num: int) -> str:
+        final = ""
+        exceptional = {900: 'CM', 400: 'CD', 90: 'XC', 40: 'XL', 9: 'IX', 4: 'IV'}
+        normal = {1000: 'M', 500: 'D', 100: 'C', 50: 'L', 10: 'X', 5: 'V'}
+        for n, e in zip(normal, exceptional):
+            # print(f"num={num} n={n}  e={e}")
+            if num <= 0:
+                break
+            if num == n:
+                print(f"num == n: {num}>{n} normal[n]={normal[n]}")
+                num = num - n
+                final += normal[n]
+                print(f"num={num}")
+            elif num > n:
+                print(f"num > n: {num}>{n} normal[n]={normal[n]}")
+                num = num - n
+                final += normal[n]
+                print(f"num={num}")
+            if num == e:
+                print(f"num == e : {num}>{e}  exceptional[e]={exceptional[e]}")
+                num = num - e
+                final += exceptional[e]
+                print(f"num={num}")
+            elif num > e:
+                if num == n or num > n:
+                    num = num - n
+                    final += normal[n]
+                else:
+                    print(f"num > e : {num}>{e}  exceptional[e]={exceptional[e]}")
+                    num = num - e
+                    final += exceptional[e]
+                    print(f"num={num}")
+            if num <= 3:
+                if num == 3:
+                    final += "III"
+                elif num == 2:
+                    final += "II"
+                elif num == 1:
+                    final += "I"
+                break
+        return final
+
+
+print(Solution.intToRoman(31))
